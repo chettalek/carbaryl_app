a785e486195b574063d9ee53a2d9c6833e345f8c
chettalek <110464221+chettalek@users.noreply.github.com>
2024-08-23 10:33:17 +0700

    1
    1

--- lib/home.dart
+++ lib/home.dart
@@ -15,7 +15,8 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   double mm = 0;
   double CC = 0;
-
+  bool _pressedAna = false;
+  bool _pressedSet = false;
   @override
   void initState() {
     super.initState();
@@ -33,91 +34,126 @@ class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Analysis()))
-                    .then((Commu) {
-                  print("object");
-                });
-                ;
-              },
-              child: Container(
-                height: 60,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 83, 213, 150),
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Analysis ',
-                              style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold)),
-                          Icon(
-                            Icons.auto_graph,
-                            size: 40,
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          ),
-                        ],
-                      ),
-                    ],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset('assets/images/Food.png'),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Analysis()))
+                      .then((Commu) {
+                    print("object");
+                  });
+                  ;
+                },
+                onTapDown: (_) {
+                  setState(() {
+                    _pressedAna = true;
+                  });
+                },
+                onTapUp: (_) {
+                  setState(() {
+                    _pressedAna = false;
+                  });
+                },
+                onTapCancel: () {
+                  setState(() {
+                    _pressedAna = false;
+                  });
+                },
+                child: AnimatedContainer(
+                  height: 60,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      color: _pressedAna
+                          ? Colors.green[700]
+                          : Color.fromARGB(255, 83, 213, 150),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  duration: Duration(milliseconds: 100),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Analysis ',
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => SettingPage(
-                              title: 'Setting',
-                            ))).then((val) {
-                  getdata();
-                });
-              },
-              child: Container(
-                height: 60,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 83, 213, 150),
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Setting ',
-                              style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold)),
-                          Icon(
-                            Icons.settings,
-                            size: 40,
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          ),
-                        ],
-                      ),
-                    ],
+              SizedBox(
+                height: 0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => SettingPage(
+                                title: 'Setting',
+                              ))).then((val) {
+                    getdata();
+                  });
+                },
+                onTapDown: (_) {
+                  setState(() {
+                    _pressedSet = true;
+                  });
+                },
+                onTapUp: (_) {
+                  setState(() {
+                    _pressedSet = false;
+                  });
+                },
+                onTapCancel: () {
+                  setState(() {
+                    _pressedSet = false;
+                  });
+                },
+                child: AnimatedContainer(
+                  height: 60,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      color: _pressedSet
+                          ? Colors.green[700]
+                          : Color.fromARGB(255, 83, 213, 150),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  duration: Duration(milliseconds: 100),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Setting ',
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Image.asset('assets/images/MSN.png'),
+              Image.asset('assets/images/Carbarly.png'),
+            ],
+          ),
         ),
       ),
     );
